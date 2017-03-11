10900a1c162430e61d95f320f38bc1608742f403
Michael Andersen <michael@steelcode.com>
2017-03-11 14:43:52 -0800

    bw2bc: replace default genesis

--- core/genesis.go
+++ core/genesis.go
@@ -309,11 +309,12 @@ func GenesisBlockForTesting(db ethdb.Database, addr common.Address, balance *big
 func DefaultGenesisBlock() *Genesis {
 	return &Genesis{
 		Config:     params.MainnetChainConfig,
-		Nonce:      66,
-		ExtraData:  hexutil.MustDecode("0x11bbe8db4e347b4e8c937c1c8370e4b5ed33adb3db69cbdb7a38e1e50b1b82fa"),
-		GasLimit:   5000,
-		Difficulty: big.NewInt(17179869184),
+		Nonce:      0xdeadbeefdeadbeef,
+		ExtraData:  hexutil.MustDecode("0x0"),
+		GasLimit:   0x8000000,
+		Difficulty: big.NewInt(200000),
 		Alloc:      decodePrealloc(mainnetAllocData),
+		Coinbase:   common.HexToAddress("0x3333333333333333333333333333333333333333"),
 	}
 }
 
